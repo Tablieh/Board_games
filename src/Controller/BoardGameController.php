2625b9e14b6e7612b9a4fb3f732229c9ab220e14
tablieh <waledo.1997@hotmail.com>
2023-03-16 12:50:43 +0100

    logo & jolie navbar :)

---
+++ src/Controller/BoardGameController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\HttpClient\BGAHttpClient;
+
+class BoardGameController extends AbstractController
+{
+    #[Route('/board/game', name: 'app_board_game')]
+    public function index(): Response
+    {
+        return $this->render('board_game/index.html.twig', [
+            'controller_name' => 'BoardGameController',
+        ]);
+    }
+    #[Route('/games' , name: 'app_games', methods: ['POST'])]
+    public function displayGames(BGAHttpClient $bga , Request $request){
+        $search = $request->request->get('searchValue');
+        return new Response($bga->getGames($search));
+    }
+    #[Route('/game' , name: 'app_game', methods: ['POST'])]
+    public function displayGame(BGAHttpClient $bga , Request $request){
+        $search = $request->request->get('gameId');
+        return new Response($bga->getGame($search));
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,28 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\HttpClient\BGAHttpClient;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig', []);
     }
-    #[Route('/games' , name: 'app_games', methods: ['POST'])]
-    public function displayGames(BGAHttpClient $bga , Request $request){
-        $search = $request->request->get('searchValue');
-        return new Response($bga->getGames($search));
-    }
-    #[Route('/game' , name: 'app_game', methods: ['POST'])]
-    public function displayGame(BGAHttpClient $bga , Request $request){
-        $search = $request->request->get('gameId');
-        return new Response($bga->getGame($search));
-    }
+    
 }
