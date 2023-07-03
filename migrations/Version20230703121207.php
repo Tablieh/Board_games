d70a6c6ce2292c70c0a440087bff12231a6e3bcf
tablieh <waledo.1997@hotmail.com>
2023-07-03 14:12:51 +0200

    report working on it

---
+++ migrations/Version20230703121207.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230703121207 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE report ADD issue_name VARCHAR(255) NOT NULL, ADD issue_description LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE report DROP issue_name, DROP issue_description');
+    }
+}
